baf6ac8d7b8437bfd9c38fc51c224bf707939661
Durgaprasad245 <136949201+Durgaprasad245@users.noreply.github.com>
2024-03-04 23:35:57 -0600

    Add files via upload

---
+++ system prog Homework 3/dnuthala.c
@@ -0,0 +1,252 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <dirent.h>
+#include <time.h>
+#include <sys/wait.h>
+
+#define TRUE 1
+#define FALSE 0
+#define MAX_PATH_SIZE 2000
+
+typedef struct Flags {
+    int is1;
+    int is2;
+    int arg2;
+    int is3;
+    char *arg3;
+    int depth;
+    int is4;
+    char *arg4;
+    int is_e;
+    char *e_command;
+    int is_E;
+    char *E_command;
+} Flags;
+
+typedef struct {
+    char **files;
+    size_t used;
+    size_t size;
+} Files;
+
+Flags flags;
+Files files;
+
+Flags* init_Flags(Flags *flags) {
+    flags->is1 = FALSE;
+    flags->is2 = FALSE;
+    flags->arg2 = 0;
+    flags->is3 = FALSE;
+    flags->arg3 = NULL;
+    flags->depth = 0;
+    flags->is4 = FALSE;
+    flags->arg4 = NULL;
+    flags->is_e = FALSE;
+    flags->e_command = NULL;
+    flags->is_E = FALSE;
+    flags->E_command = NULL;
+    return flags;
+}
+
+void initArray(Files *a, size_t initialSize) {
+    a->files = malloc(initialSize * sizeof(char *));
+    a->used = 0;
+    a->size = initialSize;
+}
+
+void insertArray(Files a, char element) {
+    if (a.used == a.size) {
+        a.size *= 2;
+        a.files = realloc(a.files, a.size * sizeof(char *));
+    }
+    if (element != '\0') {
+        a.files[a.used] = (char *) malloc(strlen(&element) + 1);
+        strcpy(a.files[a.used++], &element);
+    } else {
+        a.files[a.used] = (char *) malloc(strlen("NULL") + 1);
+        a.files[a.used++] = &element;
+    }
+}
+
+void freeArray(Files *a) {
+    free(a->files);
+    a->files = NULL;
+    a->used = a->size = 0;
+}
+
+int check_File(char *fileName, struct stat *stats, int tabSpaces) {
+    if (flags.is2 && !(stats->st_size >= flags.arg2)) {
+        return FALSE;
+    }
+    if (flags.is3 && (strstr(fileName, flags.arg3) == NULL || !(tabSpaces <= flags.depth))) {
+        return FALSE;
+    }
+    if (flags.is4 && (strcmp(flags.arg4, "f") == 0) && ((stats->st_mode & S_IFMT) != S_IFREG)) {
+        return FALSE;
+    }
+    if (flags.is4 && (strcmp(flags.arg4, "d") == 0) && ((stats->st_mode & S_IFMT) != S_IFDIR)) {
+        return FALSE;
+    }
+    return TRUE;
+}
+
+char** parse(char *command, char *fileName) {
+    char **array = malloc(1 * sizeof(char *));
+    char* dupCommand = malloc(strlen(command) * sizeof(char *));
+    strcpy(dupCommand, command);
+    char *p = strtok(dupCommand, " ");
+
+    while (p != NULL) {
+        if (files.used > 0) {
+            array = realloc(array, (files.size) * sizeof(char *));
+        }
+        array[files.used] = (char *) malloc(strlen(p) + 1);
+        strcpy(array[files.used++], p);
+        files.size *= 2;
+        p = strtok(NULL, " ");
+    }
+
+    if (fileName) {
+        files.size *= 2;
+        array = realloc(array, (files.size) * sizeof(char *));
+        array[files.used] = (char *) malloc(strlen(fileName) + 1);
+        strcpy(array[files.used++], fileName);
+        files.size *= 2;
+        array = realloc(array, (files.size) * sizeof(char *));
+        array[files.used] = (char *) malloc(strlen("NULL") + 1);
+        array[files.used++] = NULL;
+    }
+
+    return array;
+}
+
+void print_File(char *fileName, struct stat *stats, int *count, int tabSpaces) {
+    if (!flags.is1) {
+        printf("%*s[%d] %s\n", 4 * tabSpaces, "", *count, fileName);
+    } else {
+        if ((stats->st_mode & S_IFMT) == S_IFDIR) {
+            printf("%*s[%d] %s %ld Bytes, %o, %s\n", 4 * tabSpaces, "", *count, fileName, (long) stats->st_size, stats->st_mode & 0777, ctime(&stats->st_mtime));
+        } else {
+            printf("%*s[%d] %s %ld Bytes, %o, %s\n", 4 * tabSpaces, "", *count, fileName, (long) stats->st_size, stats->st_mode & 0777, ctime(&stats->st_mtime));
+        }
+    }
+    *(count) += 1;
+}
+
+void execute_command(char *command, char *fileName) {
+    pid_t pid;
+    int status;
+    pid = fork();
+    if (pid == 0) { 
+        char **args = parse(command, fileName);
+        execvp(args[0], args);
+        perror("execvp"); 
+        exit(EXIT_FAILURE);
+    } else if (pid < 0) { 
+        perror("fork");
+    } else { 
+        while (wait(&status) != pid) ; 
+    }
+}
+
+void traverse_Directory(char *path, int tabSpaces, void (*funcPtr)(char *, struct stat *, int *, int)) {
+    struct dirent *dirent;
+    struct stat stats;
+    DIR *parentDir;
+
+    if (tabSpaces == 0) {
+        printf("Starting Directory: %s\n", path);
+    }
+
+    parentDir = opendir(path);
+    if (parentDir == NULL) {
+        printf("Error opening directory '%s'\n", path);
+        exit(-1);
+    }
+
+    int count = 1;
+    while ((dirent = readdir(parentDir)) != NULL) {
+        if (strcmp(dirent->d_name, ".") == 0 || strcmp(dirent->d_name, "..") == 0) {
+            continue;
+        }
+
+        char *filePath = (char *) malloc(MAX_PATH_SIZE);
+        strcpy(filePath, path);
+        strcat(filePath, "/");
+        strcat(filePath, dirent->d_name);
+
+        if (stat(filePath, &stats) == -1) {
+            printf("Error with stat '%s\n", filePath);
+            exit(-1);
+        }
+
+        if (check_File(dirent->d_name, &stats, tabSpaces)) {
+            if (flags.is_e && flags.e_command) {
+                execute_command(flags.e_command, filePath);
+            } else if (flags.is_E && flags.E_command) {
+                insertArray(files, *dirent->d_name);
+            } else {
+                funcPtr(filePath, &stats, &count, tabSpaces);
+            }
+        }
+
+        if (dirent->d_type == DT_DIR) {
+            traverse_Directory(filePath, tabSpaces + 1, funcPtr);
+        }
+
+        free(filePath);
+    }
+    closedir(parentDir);
+
+    if (flags.is_E && flags.E_command) {
+        char **args = parse(flags.E_command, NULL);
+        args[files.used] = NULL;
+        execvp(args[0], args);
+        perror("execvp");
+        exit(EXIT_FAILURE);
+    }
+}
+
+int main(int argc, char **argv) {
+    int opt;
+    init_Flags(&flags);
+    initArray(&files, 1);
+    int opc = 0;
+    for (int i = 1; i < argc; i++) {
+        if (!flags.is1 && (strcmp(argv[i], "-v") == 0)) {
+            opc = opc + 1;
+            flags.is1 = TRUE;
+        } else if (!flags.is2 && (strcmp(argv[i], "-L") == 0)) {
+            opc = opc + 2;
+            flags.is2 = TRUE;
+            flags.arg2 = atoi(argv[i + 1]);
+        } else if (!flags.is3 && (strcmp(argv[i], "-s") == 0)) {
+            opc = opc + 3;
+            flags.is3 = TRUE;
+            flags.arg3 = argv[i + 1];
+            flags.depth = atoi(argv[i + 2]);
+        } else if (!flags.is4 && (strcmp(argv[i], "-t") == 0)) {
+            opc = opc + 2;
+            flags.is4 = TRUE;
+            flags.arg4 = argv[i + 1];
+        } else if (!flags.is_e && (strcmp(argv[i], "-e") == 0)) {
+            flags.is_e = TRUE;
+            flags.e_command = argv[i + 1];
+        } else if (!flags.is_E && (strcmp(argv[i], "-E") == 0)) {
+            flags.is_E = TRUE;
+            flags.E_command = argv[i + 1];
+        }
+    }
+
+    if (opc == argc - 1) {
+        traverse_Directory(".", 0, &print_File);
+    } else {
+        traverse_Directory(argv[argc - 1], 0, &print_File);
+    }
+
+    return 0;
+}
